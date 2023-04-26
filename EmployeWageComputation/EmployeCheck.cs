55ce0bf701abc1e37d6ea22b438b763ca68c1c6d
Mayur Sonkusare <mayursonkusare1155@gmail.com>
2023-04-26 09:10:51 +0530

    added Part Time Employee

--- EmployeWageComputation/EmployeCheck.cs
+++ EmployeWageComputation/EmployeCheck.cs
@@ -12,18 +12,24 @@ namespace EmployeWageComputation
         {
             int EMP_RATE_PER_HOUR = 20;
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int empHour = 0;
             int empWage = 0;
             Random rdm = new Random();
-            int empCheck = rdm.Next(2);
+            int empCheck = rdm.Next(3);
+            Console.WriteLine("random value : " + empCheck);
             if (empCheck == IS_FULL_TIME)
             {
                 empHour = 8;
             }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHour = 4;
+
+            }
             else
             {
                 empHour = 0;
-
             }
             empWage = empHour*EMP_RATE_PER_HOUR;
             Console.WriteLine("Employee Wage : " + empWage );
